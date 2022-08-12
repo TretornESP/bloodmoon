164e21f8b08c32ca6c5f18898ce1b5d5be83aab8
TretornESP <damebandakl@gmail.com>
2022-08-12 21:12:56 +0200

    more verbose tests

---
+++ debug.gdb
@@ -0,0 +1,5 @@
+file /mnt/c/Users/85562/lime/build/bin/kernel.elf
+target remote 172.22.96.1:1234
+set disassembly-flavor intel
+b _start
+c
--- src/test/allocatest.c
+++ src/test/allocatest.c
@@ -10,7 +10,7 @@ void allocatest_bench_crash() {
     dbg_print("This test should crash, note the number of requests and fill MAX_ALLOCS_FOR_512MB");
     dbg_print("Total available memory  is the crash number times 0x1000\n");
     dbg_print("Calculate the percentage out of your total physical memory\n");
-    
+
     while (1) request_page();
 }
 
@@ -38,7 +38,7 @@ void allocatest() {
             last_addr = (uint64_t)addr;
         }
 
-        dbg_print("Last address: 0x");
+        dbg_print("Last filled address: 0x");
         dbg_print(itoa(last_addr, 16));
         dbg_print("\nMemory at that address is now 0x");
         dbg_print(itoa(*(uint8_t*)last_addr, 16));
@@ -46,7 +46,7 @@ void allocatest() {
         dbg_print("\nTesting memory content for 0x69\n");
         for (uint64_t i = 0; i < ((MAX_ALLOCS_FOR_512MB) << 12); i++) {
             uint8_t * ptr = (uint8_t*)((uint64_t)first_addr + i);
-
+            last_addr = (uint64_t)ptr;
             if (*ptr != 0x69) {
                 dbg_print("Target is at: 0x");
                 dbg_print(itoa((MAX_ALLOCS_FOR_512MB << 12), 16));
@@ -65,10 +65,15 @@ void allocatest() {
             }
         }
 
+        dbg_print("\n");
+        dbg_print("Last checked address for 0x69: 0x");
+        dbg_print(itoa(last_addr, 16));
+        dbg_print("\n");
 
         dbg_print("\nTesting bitfield for all lock\n");
 
         for (uint64_t i = 0; i < (MAX_ALLOCS_FOR_512MB >> 3); i++) {
+            last_addr = (uint64_t)&memory->bitfield[i];
             if (memory->bitfield[i] != 0xff) {
                 dbg_print("Target is at: 0x");
                 dbg_print(itoa((MAX_ALLOCS_FOR_512MB >> 3), 16));
@@ -83,6 +88,12 @@ void allocatest() {
                 panic("Memory leak detected!");
             }
         }
+
+        dbg_print("\n");
+        dbg_print("Last checked address for bitfield lock: 0x");
+        dbg_print(itoa(last_addr, 16));
+        dbg_print("\n");
+
         dbg_print("Freeing memory\n");
         for (uint64_t i = 0; i < MAX_ALLOCS_FOR_512MB; i++) {
             void* addr = (void*)((uint64_t)first_addr + (i << 12));
@@ -96,7 +107,9 @@ void allocatest() {
         dbg_print(itoa(memory->bitfield[last_addr >> 15], 16));
         dbg_print("\n");
         dbg_print("\nTesting bitfield for all free\n");
+        
         for (uint64_t i = 6; i < (memory->total_available_pages >> 3); i++) {
+            last_addr = (uint64_t)&memory->bitfield[i];
             if (memory->bitfield[i] != 0) {
                 dbg_print("Target is at: 0x");
                 dbg_print(itoa((memory->total_available_pages >> 3), 16));
@@ -113,13 +126,22 @@ void allocatest() {
                 panic("Memory leak detected!");
             }
         }
+        dbg_print("\n");
+        dbg_print("Last checked address for empty bitfield: 0x");
+        dbg_print(itoa(last_addr, 16));
+        dbg_print("\n");
         dbg_print("Testing memory content for 0x0\n");
-        for (uint64_t i = 0; i < MAX_ALLOCS_FOR_512MB << 12; i++) {
+        for (uint64_t i = 0; i < ((MAX_ALLOCS_FOR_512MB) << 12); i++) {
             uint8_t * ptr = (uint8_t*)((uint64_t)first_addr + i);
+            last_addr = (uint64_t)ptr;
             if (*ptr != 0x00) {
                 panic("Memory leak detected!");
             }
         }
+        dbg_print("\n");
+        dbg_print("Last checked address for 0x0: 0x");
+        dbg_print(itoa(last_addr, 16));
+        dbg_print("\n");
     }
 
     dbg_print("[SUCCESS] !!!Total lock unlock test passed!!! [SUCCESS]\n");    
