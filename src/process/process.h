b40214f83726db9d2414db06cb1a04bc002f6da1
Xabier <xabier.iglesias.perez@udc.es>
2023-03-15 18:57:23 +0100

    cuak

--- src/process/process.h
+++ src/process/process.h
@@ -55,14 +55,75 @@ struct descriptors {
     uint8_t stderr;
 };
 
+struct vm_area_struct {
+    struct mm_struct *vm_mm;        /* The address space we belong to. */
+    unsigned long vm_start;         /* Our start address within vm_mm. */
+    unsigned long vm_end;           /* The first byte after our end address
+                                       within vm_mm. */
+    struct vm_area_struct *vm_next; /* linked list of VM areas per task */
+    unsigned long vm_flags;         /* Flags, listed below. */
+    int file;
+};
+
+struct mm_struct {
+        struct vm_area_struct  *mmap;               /* list of memory areas */
+        unsigned long long     pgd;                /* page global directory */
+        int                    mm_users;            /* address space users */
+        int                    map_count;           /* number of memory areas */
+
+        unsigned long          start_code;          /* start address of code */
+        unsigned long          end_code;            /* final address of code */
+        unsigned long          start_data;          /* start address of data */
+        unsigned long          end_data;            /* final address of data */
+        unsigned long          start_brk;           /* start address of heap */
+        unsigned long          brk;                 /* final address of heap */
+        unsigned long          start_stack;         /* start address of stack */
+        unsigned long          arg_start;           /* start of arguments */
+        unsigned long          arg_end;             /* end of arguments */
+        unsigned long          env_start;           /* start of environment */
+        unsigned long          env_end;             /* end of environment */
+
+        unsigned long          rss;                 /* pages allocated */
+        unsigned long          total_vm;            /* total number of pages */
+        unsigned long          locked_vm;           /* number of locked pages */
+};
+
 struct task {
+    volatile long state;
+    unsigned long flags;
+    int sigpending;
+    
+    long nice;
+    struct mm_struct *mm;
+    int processor;
+    unsigned long sleep_time;
+    int exit_code, exit_signal;
+    int pdeath_signal;
+
     int16_t pid;
-    int16_t ppid;
-    uint8_t priority;
-    uint8_t status;
+    int16_t pgrp;
+
+    struct task_struct *parent;
+
+    int16_t uid;
+    int16_t euid;
+    int16_t suid;
+    int16_t gid;
+    int16_t egid;
+    int16_t sgid;
+    int16_t fsuid;
+    int16_t fsgid;
+    
+    struct tty_struct *tty;
+    unsigned int locks;
+
+    int * open_files;
+
     void * entry;
     CPU_CONTEXT* context;
     struct descriptors* descriptors;
+    struct task *next_task, *prev_task;
+
 };
 
 void kyieldtest();
@@ -71,4 +132,5 @@ extern void setContext(CPU_CONTEXT*);
 extern void getContext(CPU_CONTEXT*);
 extern void setContextNoRip(CPU_CONTEXT*);
 void dump_context(CPU_CONTEXT*);
+
 #endif
